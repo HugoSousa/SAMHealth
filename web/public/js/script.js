0cc657464a51629bedc8e3bb79a7a8a659ae6dda
Ricardo Silva <ricardani@gmail.com>
2015-11-10 19:31:13 +0000

    Search field done

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -4,7 +4,11 @@ $( document ).ready(function() {
     //the positioning after collapsing an accordion item is not correct
     $('.collapse').on('show.bs.collapse hide.bs.collapse', function(e) {
         e.preventDefault();
-        
+        var caret_span = $(this).parent().find('.caret-span');
+        if(caret_span.hasClass('glyphicon-chevron-down'))
+        	caret_span.removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        else if(caret_span.hasClass('glyphicon-chevron-up'))
+        	caret_span.removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
     });
     $('[data-toggle="collapse"]').on('click', function(e) {
         e.preventDefault();
@@ -21,76 +25,14 @@ $( document ).ready(function() {
     $("#sbtn").click(function() {
   		console.log("clicked button. send get request");
 
-  		var query_term = $("#searchbox").val();
-  		var results_per_page = 10;
-  		var page = 1;
-  		var query = 'text:' + query_term;
+  		var query = $("#searchbox").val();
+
+  		window.location.href = 'http://localhost/search?query=' + query;
+
   		//?q=text:sim&fl=id,patient,therapist,session_number,session_date,score&wt=json&hl=true&hl.snippets=20&hl.fl=content&hl.usePhraseHighlighter=true'
   		
   		//to count the number of times a term appears on the document (a term is a single word! - if multiple terms, they can be calculated separately): 
   		//add to fl: <FIELD_ALIAS>:termfreq(text,'<TERM>')
-
-  		$.ajax({
-	    	'url' : 'http://localhost:8983/solr/samh/select',
-		    'type' : 'GET',
-		    'data' : {
-		    	'q' : query_term,
-		    	'fl' : 'id,patient,therapist,session_number,session_date',
-		    	'wt' : 'json',
-		    	'hl' : 'true',
-		    	'hl.snippets': 20,
-		    	'hl.fl' : 'content',
-		    	'hl.usePhraseHighlighter' : 'true',
-		    	'rows' : results_per_page,
-		    	'start' : (page - 1) * results_per_page
-		    },
-		    'dataType' : 'jsonp',
-		    'jsonp' : 'json.wrf',
-		    'success' : function(data) {
-		    	console.log('success');
-		    	//console.log(JSON.stringify(data));
-		    	var results_found = data.response.numFound;
-		    	var pages = Math.ceil(results_found / results_per_page); 
-		    	console.log("FOUND: " + results_found);
-		    	console.log("PAGES: "+ pages);
-		    	console.log(data);
-
-		    	//if no documents returned, add "no documents returned"
-		    	if(results_found == 0){
-		    		$('#no-results').removeClass("hidden");
-		    	}else{
-		    		$('#no-results').addClass("hidden");
-			    	$('#results-accordion').children('.panel').each(function (index, element) {
-
-			    		$(element).addClass("hidden");
-			    		if(data.response.docs[index] != undefined){
-			    			$(element).removeClass("hidden");
-						
-			    			//if term, say number of ocurrences
-			    			//show score
-
-				    		$(element).find('.patient').text(data.response.docs[index].patient);
-				    		$(element).find('.therapist').text(data.response.docs[index].therapist);
-				    		$(element).find('.session').text(data.response.docs[index].session_number);
-				    		var result_index = (page - 1) * results_per_page + index + 1;
-				    		$(element).find('.result-index').text('#' + result_index);
-				    		if(data.response.docs[index].session_date != undefined){
-				    			$(element).find('.date').text(data.response.docs[index].session_date);
-				    		}else{
-				    			$(element).find('.date-p').addClass('hidden');
-				    		}
-				    	}
-
-			    		//$('.panel-title a').text("AAA");
-			    		//$('.panel-body').text("BBB");
-
-					});
-			    }
-	    	},
-	    	'error' : function(data) {
-		    	console.log('error');
-	    	}
-	  	});
 		
 	});
 });
--- web/public/templates/search.php
+++ web/public/templates/search.php
@@ -2,5 +2,11 @@
 include('base_templates/header.php');
 include('base_templates/search_bar.php');
 include('base_templates/results.php');
+
+?>
+<script>
+	var GLOBAL_QUERY = "<?php echo $_GET['query']; ?>";
+</script>
+<?php
 include('base_templates/footer.php');
 ?>
